7adcba9b0bcdc85023f289294fc75ac661bc962e
TiThuthur <thurthur.petit.py@gmail.com>
2024-04-17 20:45:25 +0200

    il faut maintenant actualiser le score

--- js/main.js
+++ js/main.js
@@ -1,24 +1,65 @@
 const wordSpace = document.getElementById("js-word");
-const wordList = ["saucisson"];
-// const wordList = [
-//   "jambon",
-//   "saucisson",
-//   "ordinateur",
-//   "ecran",
-//   "pain",
-//   "programmation",
-// ];
+const lettersInputs = document.querySelectorAll(".letter-button");
+const lettersUsed = document.querySelector(".letter-used");
+let score = 11;
+const wordList = [
+  "jambon",
+  "saucisson",
+  "ordinateur",
+  "ecran",
+  "pain",
+  "programmation",
+];
 
+const getWord = (wordList) => {
+  const index = Math.floor(Math.random() * wordList.length);
+  return wordList[index];
+};
+
+const hideLetter = (word) => {
+  const hideWord = Array.from(word);
+  console.log(word);
+  for (const index in hideWord) {
+    hideWord[index] = "_";
+  }
+  console.log(hideWord);
+  return hideWord;
+};
+const renderWord = (wordList) => {
+  wordSpace.innerText = wordList.join("");
+};
+const happendLetter = (letter) => {
+  const letterElement = document.createElement("div");
+  letterElement.innerHTML = letter.toUpperCase();
+  lettersUsed.append(letterElement);
+};
+const checkWord = (letter, wordList) => {
+  let indice = [];
+  let index = wordList.indexOf(letter);
+  while (index != -1) {
+    indice.push(index);
+    index = wordList.indexOf(letter, index + 1);
+  }
+  return indice;
+};
+const letterList = [];
 const word = Array.from(getWord(wordList));
 
 let hideWord = hideLetter(word);
-
-checkWord("s", word);
-
-let indice = [];
-let index;
-while(index!=1) {
-indice.push(index);
-index = word.indexOf("s", index +1);
+console.log(word.join(""));
+renderWord(hideWord);
+for (const input of lettersInputs) {
+  input.addEventListener("click", (e) => {
+    let letter = e.target.id.toLowerCase();
+    const indice = checkWord(letter, word);
+    for (const i of indice) {
+      console.log(i);
+      hideWord[i] = letter;
+    }
+    renderWord(hideWord);
+    if (letterList.find((element) => element === letter) === undefined) {
+      happendLetter(letter);
+    }
+    letterList.push(letter);
+  });
 }
-console.log(indice);
